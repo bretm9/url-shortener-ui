019df27dd3591fecfc9eaccc1fb2b8f1e0468320
Bret Merritt <merrittbret9@gmail.com>
2020-11-10 09:31:40 -0700

    add postUrl

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,15 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (url, title) => {
+  const init = { 
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    body: {
+      long_url: url, title: title
+    }
+  }
+  return fetch('http://localhost:3001/api/v1/urls', init)
+    .then(response => response.json())
+}
